be044bba3f0b3f08ad21b5bc70acfd1bc8ea3af2
Rickard Åberg <rickard.aaberg@gmail.com>
2023-06-09 22:09:15 +0200

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'prettier',
+  ],
+  'rules': {
+  },
+};
+
